f914f7296404ea8e4713457aded926c7305fb782
5ym <16yuim@gmail.com>
2020-08-21 16:09:58 +0900

    token取得機能まで実装

--- WCUPP.php
+++ WCUPP.php
@@ -11,81 +11,222 @@
  /*
  * This action hook registers our PHP class as a WooCommerce payment gateway
  */
-add_filter( 'woocommerce_payment_gateways', 'wcupp_add_gateway_class' );
-function wcupp_add_gateway_class( $gateways ) {
-	$gateways[] = 'WC_Wcupp_Gateway'; // your class name is here
+add_filter( 'woocommerce_payment_gateways', 'Univapay_add_gateway_class' );
+function univapay_add_gateway_class( $gateways ) {
+	$gateways[] = 'WC_Univapay_Gateway'; // your class name is here
 	return $gateways;
 }
  
 /*
  * The class itself, please note that it is inside plugins_loaded action hook
  */
-add_action( 'plugins_loaded', 'Wcupp_init_gateway_class' );
-function wcupp_init_gateway_class() {
+add_action( 'plugins_loaded', 'Univapay_init_gateway_class' );
+function Univapay_init_gateway_class() {
  
-	class WC_Wcupp_Gateway extends WC_Payment_Gateway {
+	class WC_Univapay_Gateway extends WC_Payment_Gateway {
  
  		/**
  		 * Class constructor, more about it in Step 3
  		 */
  		public function __construct() {
- 
-		...
- 
+            $this->id = 'wcupp'; // payment gateway plugin ID
+            $this->icon = ''; // URL of the icon that will be displayed on checkout page near your gateway name
+            $this->has_fields = true; // in case you need a custom credit card form
+            $this->method_title = 'Univapay Gateway';
+            $this->method_description = 'Description of Univapay payment gateway'; // will be displayed on the options page
+         
+            // gateways can support subscriptions, refunds, saved payment methods,
+            // but in this tutorial we begin with simple payments
+            $this->supports = array(
+                'products'
+            );
+         
+            // Method with all the options fields
+            $this->init_form_fields();
+         
+            // Load the settings.
+            $this->init_settings();
+            $this->title = $this->get_option( 'title' );
+            $this->description = $this->get_option( 'description' );
+            $this->enabled = $this->get_option( 'enabled' );
+            $this->testmode = 'yes' === $this->get_option( 'testmode' );
+            $this->publishable_key = $this->get_option( 'publishable_key' );
+         
+            // This action hook saves the settings
+            add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+         
+            // We need custom JavaScript to obtain a token
+            add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
+         
+            // You can also register a webhook here
+            // add_action( 'woocommerce_api_{webhook name}', array( $this, 'webhook' ) ); 
  		}
  
 		/**
  		 * Plugin options, we deal with it in Step 3 too
  		 */
  		public function init_form_fields(){
- 
-		...
- 
+            $this->form_fields = array(
+                'enabled' => array(
+                    'title'       => 'Enable/Disable',
+                    'label'       => 'Enable Univapay Gateway',
+                    'type'        => 'checkbox',
+                    'description' => '',
+                    'default'     => 'no'
+                ),
+                'title' => array(
+                    'title'       => 'Title',
+                    'type'        => 'text',
+                    'description' => 'This controls the title which the user sees during checkout.',
+                    'default'     => 'Credit Card',
+                    'desc_tip'    => true,
+                ),
+                'description' => array(
+                    'title'       => 'Description',
+                    'type'        => 'textarea',
+                    'description' => 'This controls the description which the user sees during checkout.',
+                    'default'     => 'Pay with your credit card via our super-cool payment gateway.',
+                ),
+                'testmode' => array(
+                    'title'       => 'Test mode',
+                    'label'       => 'Enable Test Mode',
+                    'type'        => 'checkbox',
+                    'description' => 'Place the payment gateway in test mode using test gateway.',
+                    'default'     => 'yes',
+                    'desc_tip'    => true,
+                ),
+                'publishable_key' => array(
+                    'title'       => 'Live Store ID',
+                    'type'        => 'number'
+                )
+            );        
 	 	}
  
 		/**
 		 * You will need it if you want your custom credit card form, Step 4 is about it
 		 */
 		public function payment_fields() {
- 
-		...
- 
+            // ok, let's display some description before the payment form
+            if ( $this->description ) {
+                // you can instructions for test mode, I mean test card numbers etc.
+                if ( $this->testmode ) {
+                    $this->description .= ' TEST MODE ENABLED. In test mode, you can use the card numbers listed in <a href="#" target="_blank" rel="noopener noreferrer">documentation</a>.';
+                    $this->description  = trim( $this->description );
+                }
+                // display the description with <p> tags etc.
+                echo wpautop( wp_kses_post( $this->description ) );
+            }
+        
+            // Add this action hook if you want your custom payment gateway to support it
+            do_action( 'woocommerce_credit_card_form_start', $this->id );
+            // I recommend to use inique IDs, because other gateways could already use #ccNo, #expdate, #cvc
+            echo '<p>言語：<select id="lang">  
+            <option value="ja">日本語（ja）</option>  
+            <option value="en">英語（en）</option>  
+            <option value="cn">中国語簡体字 （cn）</option>  
+            <option value="tw">中国語繁体字（tw）</option>  
+            </select></p>
+            <p>カード番号：  <input type="text" id="cardno"></p>
+            <p>セキュリティコード：  <input type="text" id="securitycode"></p>
+            <p>カード有効期限：  <input type="text" id="expire_month">/<input type="text" id="expire_year"></p>
+            <p>カード名（名）：  <input type="text" id="holderfirstname">   
+            カード名（姓）：  <input type="text" id="holderlastname"></p>
+            <input type="hidden" name="upcmemberid" id="upcmemberid">';
+            do_action( 'woocommerce_credit_card_form_end', $this->id );
 		}
  
 		/*
 		 * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
 		 */
 	 	public function payment_scripts() {
- 
-		...
- 
+            global $user_ID;
+             // we need JavaScript to process a token only on cart/checkout pages, right?
+            if ( ! is_cart() && ! is_checkout() && ! isset( $_GET['pay_for_order'] ) ) {
+                return;
+            }
+            // if our payment gateway is disabled, we do not have to enqueue JS too
+            if ( 'no' === $this->enabled ) {
+                return;
+            }
+            // no reason to enqueue JavaScript if API keys are not set
+            if ( empty( $this->publishable_key ) ) {
+                return;
+            }
+            // do not work with card detailes without SSL unless your website is in a test mode
+            if ( ! $this->testmode && ! is_ssl() ) {
+                return;
+            }
+            // let's suppose it is our payment processor JavaScript that allows to obtain a token
+            wp_enqueue_script( 'univapay_js', 'https://token.ccps.jp/UpcTokenPaymentMini.js' );
+            // and this is our custom JS in your plugin directory that works with token.js
+            wp_register_script( 'woocommerce_univapay', plugins_url( 'univapay.js', __FILE__ ), array( 'univapay_js' ) );
+            // in most payment processors you have to use PUBLIC KEY to obtain a token
+            wp_localize_script( 'woocommerce_univapay', 'univapay_params', array(
+                'publishableKey' => $this->publishable_key,
+                'user_ID' => $user_ID
+            ) );
+            wp_enqueue_script( 'woocommerce_univapay' );
 	 	}
  
 		/*
  		 * Fields validation, more in Step 5
 		 */
 		public function validate_fields() {
- 
-		...
- 
 		}
  
 		/*
 		 * We're processing the payments here, everything about it is in Step 5
 		 */
 		public function process_payment( $order_id ) {
- 
-		...
- 
+            global $woocommerce;
+            // we need it to get any order detailes
+            $order = wc_get_order( $order_id );
+            /*
+              * Array with parameters for API interaction
+             */
+            $args = array();
+            /*
+             * Your API interaction could be built with wp_remote_post()
+              */
+            $sod = $this->testmode ? '&sod=testtransaction' : '';
+            var_dump($_POST);
+            $response = wp_remote_post('https://gw.ccps.jp/memberpay.aspx?sid='.$this->publishable_key.'&svid=1&ptype=1&job=CAPTURE&rt=2&upcmemberid='.$_POST['upcmemberid'].$sod.'&siam1='.$order->get_total().'&sisf1='.$order->get_total_shipping(), $args);
+         
+             if( !is_wp_error( $response ) ) {
+                 $result_array = explode('&', $response['body']);  
+                 for( $i = 0; $i < count( $result_array ); $i++ ) {  
+                    $target_array = explode( '=', $result_array[$i] );  
+                    if( $target_array[0] == "rst" ) {break;}  
+                }  
+                if ( (int)$target_array[1] == 1 ) {  
+                    /* 決済処理成功の場合はここに処理内容を記載 */  
+                    // we received the payment
+                    $order->payment_complete();
+                    $order->reduce_order_stock();
+                    // some notes to customer (replace true with false to make it private)
+                    $order->add_order_note( 'Hey, your order is paid! Thank you!', true );
+                    // Empty cart
+                    $woocommerce->cart->empty_cart();
+                    // Redirect to the thank you page
+                    return array(
+                        'result' => 'success',
+                        'redirect' => $this->get_return_url( $order )
+                    );
+                } else {  
+                    /* 決済処理失敗の場合はここに処理内容を記載 */  
+                    wc_add_notice(  'Please try again.', 'error' );
+                    return;
+                }
+            } else {
+                wc_add_notice(  'Connection error.', 'error' );
+                return;
+            }
 	 	}
  
 		/*
 		 * In case you need a webhook, like PayPal IPN etc
 		 */
 		public function webhook() {
- 
-		...
- 
 	 	}
  	}
 }
---
+++ univapay.js
@@ -0,0 +1,42 @@
+function settoken(response) {
+    if (response.resultCode !== "000" && response.resultCode !== 0) {
+        var lang = "ja";
+        if (document.getElementById('lang') != null)  
+            lang = document.getElementById('lang').value;
+        window.alert('Error: ' + getResultCodeDetail(response.resultCode, lang));
+    } else {
+        jQuery(function($) {
+            var checkout_form = $('form.woocommerce-checkout');
+            // add a token to our hidden input field
+            checkout_form.find('#upcmemberid').val(response.tokenObject.token);
+            // deactivate the tokenRequest function event
+            checkout_form.off('checkout_place_order', tokenRequest);
+            // submit the form now
+            // checkout_form.submit();
+        });
+    }
+}
+function gettoken() {
+    cardno = document.getElementById('cardno').value;  
+    securitycode = document.getElementById('securitycode').value;  
+    expire = document.getElementById('expire_month').value + '' +  
+    document.getElementById('expire_year').value;  
+    holderfirstname = document.getElementById('holderfirstname').value;  
+    holderlastname = document.getElementById('holderlastname').value;  
+    email = document.getElementById('billing_email').value;  
+    phonenumber = document.getElementById('billing_phone').value;  
+    Multipayment.init(univapay_params.publishableKey); //当社発行の店舗ID  
+    Multipayment.getMember({　//決済フォームより取得した情報  
+        cardno: cardno, //カード番号  
+        securitycode: securitycode, //セキュリティコード  
+        expire: expire, //カード有効期限  
+        holderfirstname: holderfirstname, //カードホルダー名  
+        holderlastname: holderlastname, //カードホルダー姓  
+        membercode: univapay_params.user_ID == 0 ? null : univapay_params.user_ID, //会員コード  
+        email: email, //メール  
+        phonenumber: phonenumber//電話番号  
+    }, settoken);
+}
+jQuery(function($) {
+    $('form.woocommerce-checkout').on('checkout_place_order', gettoken);
+});
