00152b46921b74c01a9d6657c865e8e963c7ff05
Roy Wijaya <r.wijaya@univapay.com>
2024-10-31 15:32:45 +0900

    fix e2e on github action

--- UnivaPay-for-WooCommerce/tests/e2e/models/wp/admin/univapay-setting.ts
+++ UnivaPay-for-WooCommerce/tests/e2e/models/wp/admin/univapay-setting.ts
@@ -7,10 +7,10 @@ class WCUnivapaySettingPage {
     formUrl: Selector
 
     constructor() {
-        this.enabled = Selector("#woocommerce_upfw_enabled")
-        this.widget = Selector("#woocommerce_upfw_widget")
-        this.api = Selector("#woocommerce_upfw_api")
-        this.formUrl = Selector("#woocommerce_upfw_formurl")
+        this.enabled = Selector("input#woocommerce_upfw_enabled")
+        this.widget = Selector("input#woocommerce_upfw_widget")
+        this.api = Selector("input#woocommerce_upfw_api")
+        this.formUrl = Selector("input#woocommerce_upfw_formurl")
     }
 }
 
