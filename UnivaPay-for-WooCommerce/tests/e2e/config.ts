e658130257efc2b42cc0d41a91faa9748f24064b
Roy Wijaya <r.wijaya@univapay.com>
2024-11-05 11:35:14 +0900

    cleanup

--- UnivaPay-for-WooCommerce/tests/e2e/config.ts
+++ UnivaPay-for-WooCommerce/tests/e2e/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    wpUrl: 'http://localhost:' + (process.env.WP_PORT || '3080'),
+    wpUrl: 'http://localhost:' + (process.env.WP_PORT || '80'),
     univapayConsoleUrl: process.env.E2E_UNIVAPAY_ADMIN_CONSOLE_URL || '',
     univapayConsoleEmail: process.env.E2E_UNIVAPAY_ADMIN_EMAIL || '',
     univapayConsolePassword: process.env.E2E_UNIVAPAY_ADMIN_PASSWORD || '',
